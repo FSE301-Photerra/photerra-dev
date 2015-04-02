d4ab583eda96e446e115de915fb0d3dd0b9e45dd
Weston Neal <wtneal@asu.edu>
2015-04-02 12:37:08 -0700

    updated database schema

--- scripts/initdb.sql
+++ scripts/initdb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 17, 2015 at 10:18 PM
+-- Generation Time: Apr 02, 2015 at 12:23 PM
 -- Server version: 5.5.41-cll-lve
 -- PHP Version: 5.4.23
 
@@ -19,8 +19,22 @@ SET time_zone = "+00:00";
 --
 -- Database: `photerras_DB`
 --
+DROP DATABASE IF EXISTS photerras_DB;
 CREATE DATABASE photerras_DB;
 USE photerras_DB
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Photos`
+--
+
+CREATE TABLE IF NOT EXISTS `Photos` (
+  `Lat` float DEFAULT NULL,
+  `Lng` float DEFAULT NULL,
+  `Title` text,
+  `Picture` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -29,18 +43,31 @@ USE photerras_DB
 --
 
 CREATE TABLE IF NOT EXISTS `Users` (
-  `member_id` text NOT NULL,
-  `username` text NOT NULL,
-  `password` text NOT NULL,
-  `email` text NOT NULL
+  `member_id` text,
+  `username` text,
+  `password` text,
+  `email` text,
+  `fname` text,
+  `lname` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Users`
 --
 
-INSERT INTO `Users` (`member_id`, `username`, `password`, `email`) VALUES
-('1122334455', 'Rphello101', 'FSE', 'aowdija');
+INSERT INTO `Users` (`member_id`, `username`, `password`, `email`, `fname`, `lname`) VALUES
+('1122334455', 'Rphello101', 'FSE', 'aowdija', NULL, NULL),
+('234233', 'testuser', '123456', 'ggggg', NULL, NULL),
+('234233', 'testuser', '123456', 'ggggg', NULL, NULL),
+('543626', 'andrew', '123456', 'fffff', NULL, NULL),
+('a8f238c703', 'test111', 'dddd', 'aherzberg5@gmail.com', 'Andrew', 'Herzberg'),
+('74b1c00bd5', 'rapratt', '123456', 'asdf@asdf', 'Reese', 'Pratt'),
+('e804db99f8', 'rapratt101', '123456', 'asdf@asdff', 'Reese3', 'Pratt3'),
+('7347a00343', '', '', '', '', ''),
+('5df6ae72b9', 'wtneal', 'S3cur3', 'wtneal@asu.edu', 'Weston', 'Neal'),
+('fe910d4212', 'Kurvani', 'fse301', 'kkeyvan1@asu.edu', 'Kurosh', 'Keyvani'),
+('f80e5d3183', 'Kurvani', 'fse301', 'kkeyvan1@asu.edu', 'Kurosh', 'Keyvani'),
+('ca205cffc1', 'phopho', '123456', 'aherzberg5@gmail.com', 'Andrew ', 'gggggg');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
