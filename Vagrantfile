f8597aec3f17ce6535c20517fda908f3df7e2775
Weston Neal <wtneal@asu.edu>
2015-04-11 11:45:42 -0700

    updated vagrantfile and sql scripts

--- Vagrantfile
+++ Vagrantfile
@@ -5,9 +5,16 @@ Vagrant.configure(2) do |config|
   config.vm.network "forwarded_port", guest: 80, host: 8080
   config.vm.synced_folder "src/", "/var/www/public", create: true
   config.vm.provision "shell", inline: <<-SHELL
-    cd /var/www && rm -rf public/*
+    cd /var/www/public && rm -rf ./*
     yum -y install git
-    git clone https://github.com/FSE301-Photerra/photerra.git public
-    mysql -u root < public/scripts/initdb.sql
+    git init
+    git remote add origin https://github.com/FSE301-Photerra/photerra.git
+    git pull origin master
+    mysql -u root < /vagrant/scripts/initdb.sql
+
+    curl -sS https://getcomposer.org/installer | php
+    mv composer.phar /usr/local/bin/composer
+
+    composer install
   SHELL
 end
--- scripts/initdb.sql
+++ scripts/initdb.sql
@@ -19,29 +19,80 @@ SET time_zone = "+00:00";
 --
 -- Database: `photerras_DB`
 --
+DROP DATABASE IF EXISTS photerras_DB;
 CREATE DATABASE photerras_DB;
-USE photerras_DB
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Users`
---
+USE photerras_DB;
 
 CREATE TABLE IF NOT EXISTS `Users` (
-  `member_id` text NOT NULL,
-  `username` text NOT NULL,
-  `password` text NOT NULL,
-  `email` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` MEDIUMINT(8) unsigned NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(250) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `firstname` VARCHAR(50) NOT NULL,
+  `lastname` VARCHAR(50) NOT NULL,
+  `salt` VARCHAR(100) NOT NULL,
+  `createdOn` TIMESTAMP DEFAULT 0,
+  `updatedOn` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY `uid` (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10000 ;
 
---
--- Dumping data for table `Users`
---
+CREATE TABLE IF NOT EXISTS `Photos` (
+  `id` MEDIUMINT(8) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` MEDIUMINT(8) unsigned NOT NULL,
+  `lat` float NOT NULL,
+  `lng` float NOT NULL,
+  `title` VARCHAR(100) NOT NULL,
+  `isPremium` BIT DEFAULT 0,
+  `filename` VARCHAR(250) NOT NULL,
+  `createdOn` TIMESTAMP DEFAULT 0,
+  PRIMARY KEY `photoid` (`id`),
+  FOREIGN KEY (`uid`) REFERENCES Users(`id`),
+  CHECK (`lat` >= -90.0 and `lat` <= 90.0),
+  CHECK (`lng` >= -180.0 and `lng` <= 180.0)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10000 ;
 
-INSERT INTO `Users` (`member_id`, `username`, `password`, `email`) VALUES
-('1122334455', 'Rphello101', 'FSE', 'aowdija');
+CREATE TABLE IF NOT EXISTS `PaymentTypes` (
+  `id` INT unsigned NOT NULL AUTO_INCREMENT,
+  `desc` varchar(150) NOT NULL,
+  `code` varchar (3) NOT NULL,
+  PRIMARY KEY `typeid` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0 ;
+
+CREATE TABLE IF NOT EXISTS `Payments` (
+  `id` MEDIUMINT(8) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` MEDIUMINT(8) unsigned NOT NULL,
+  `typeid` INT unsigned NOT NULL,
+  `createdOn` TIMESTAMP DEFAULT 0,
+  PRIMARY KEY `id` (`id`),
+  FOREIGN KEY (`uid`) REFERENCES Users(`id`),
+  FOREIGN KEY (`typeid`) REFERENCES PaymentTypes(`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10000 ;
+
+
+INSERT INTO `Users` (`username`, `password`, `email`, `firstname`, `lastname`, `salt`) VALUES
+('Rphello101', 'FSE', 'aowdija', NULL, NULL, ''),
+('testuser', '123456', 'ggggg', NULL, NULL, ''),
+('andrew', '123456', 'fffff', NULL, NULL, ''),
+('test111', 'dddd', 'aherzberg5@gmail.com', 'Andrew', 'Herzberg', ''),
+('rapratt', '123456', 'asdf@asdf', 'Reese', 'Pratt', ''),
+('rapratt101', '123456', 'asdf@asdff', 'Reese3', 'Pratt3', ''),
+('wtneal', 'S3cur3', 'wtneal@asu.edu', 'Weston', 'Neal', ''),
+('Kurvani', 'fse301', 'kkeyvan1@asu.edu', 'Kurosh', 'Keyvani', '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+INSERT INTO `Photos` (`lat`, `lng`, `title`, `filename`, `uid`) VALUES
+(50, 40, '', 'ece178fe9f', 10001),
+(50, 50, 'wwww', 'a006d32d0e', 10002);
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+INSERT INTO `PaymentTypes` (`desc`, `code`) VALUES
+('Premium Photo', 'pp'),
+('Increase Photo Limit', 'il');
